6e490136ed2fdbdbc7e83176da285221f6424265
willy1669 <williamschinonso11@gmail.com>
2019-05-20 10:53:31 +0100

    create instance of songs and add to playlist

---
+++ app.js
@@ -0,0 +1,7 @@
+var playlist = new Playlist();
+
+var hereComesTheSun = new Song("Here Comes The Sun", "The Beatles", "2:54");
+var walkingOnSunshine = new Song("walking On Sunshine", "Katrina and the waves", "3:43");
+
+playlist.add(hereComesTheSun);
+playlist.add(walkingOnSunshine);
