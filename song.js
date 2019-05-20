4033454969f0e177ac4c05c6ac356c303e5e7b57
willy1669 <williamschinonso11@gmail.com>
2019-05-20 12:31:35 +0100

    modifie toHTML function

--- song.js
+++ song.js
@@ -13,17 +13,17 @@ Song.prototype.stop = function () {
     this.isPlaying = false;
 }
 
-Song.prototype.toHtml = function () {
+Song.prototype.toHTML  = function () {
     var htmlString = '<li ';
     if (this.isPlaying) {
         htmlString += 'class="current"';
     }
     htmlString += '>';
-    htmlString += 'this.title';
-    htmlString += '-';
-    htmlString += 'this.artist';
-    htmlString += '<span class="duration">'
-    htmlString += 'this.duration'
-    htmlString += '</span></li>'
+    htmlString += this.title;
+    htmlString += ' - ';
+    htmlString += this.artist;
+    htmlString += '<span class="duration">  '
+    htmlString += this.duration;
+    htmlString += '</span></li>';
     return htmlString;
 }
