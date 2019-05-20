cf716bf8c5b4a5ca734cfc379d3aab28907c7026
willy1669 <williamschinonso11@gmail.com>
2019-05-20 12:30:30 +0100

    create function for onclick events

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ var walkingOnSunshine = new Song("walking On Sunshine", "Katrina and the waves",
 
 playlist.add(hereComesTheSun);
 playlist.add(walkingOnSunshine);
+
+var playlistElement = document.getElementById("playlist");
+
+playlist.renderInElement(playlistElement);
+
+var playButton = document.getElementById("play");
+playButton.onclick = function () {
+    playlist.play();
+    playlist.renderInElement(playlistElement);
+}
+var nextButton = document.getElementById("next");
+nextButton.onclick = function () {
+    playlist.next();
+    playlist.renderInElement(playlistElement);
+}
+var stopButton = document.getElementById("stop");
+stopButton.onclick = function () {
+    playlist.stop();
+    playlist.renderInElement(playlistElement);
+}
