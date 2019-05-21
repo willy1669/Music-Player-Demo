6035ae10811bee3884ce4f1ccd3abc146dc30d42
willy1669 <williamschinonso11@gmail.com>
2019-05-21 07:02:05 +0100

    add toHTML prototype function

--- video.js
+++ video.js
@@ -1,5 +1,22 @@
-function Video (year) {
+function Video (title, year, duration) {
     Media.call(this, title, duration);
     this.year = year;
 }
 
+Video.prototype = Object.create(Media.prototype);
+
+Video.prototype.toHTML  = function () {
+    var htmlString = '<li ';
+    if (this.isPlaying) {
+        htmlString += 'class="current"';
+    }
+    htmlString += '>';
+    htmlString += this.title;
+    htmlString += ' (';
+    htmlString += this.year;
+    htmlString += ') ';
+    htmlString += '<span class="duration">  '
+    htmlString += this.duration;
+    htmlString += '</span></li>';
+    return htmlString;
+}
